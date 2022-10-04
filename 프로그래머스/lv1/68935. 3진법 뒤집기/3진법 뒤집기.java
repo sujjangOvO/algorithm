ae76a3211f64b186932a2dda6f21ea96cdd17e81
sujjangOvO <89267864+sujjangOvO@users.noreply.github.com>
2022-10-04 15:15:55 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.07 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -5,12 +5,13 @@ class Solution {
         int answer = 0;
         ArrayList<Integer> temp = new ArrayList<>();
         
+        // 1억 미만인 수라 이렇게 했지 n이 더 커지면 진법의 원리에 맞게 해야 함
         while(n>0){
             temp.add(n%3);
             n /= 3;
         }
         
-        //Collections.reverse(temp);
+        //Collections.reverse(temp); 어차피 뒤집혀서 들어가있기 때문에 뒤집어줄 필요가 없다.
         
         for(int i=1; i<=temp.size(); i++){
             // System.out.println((int)Math.pow(3, temp.size()-i) * temp.get(i-1));
