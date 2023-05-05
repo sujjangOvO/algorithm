8af8a65f6b3d88a5eb8ae77e2322073c1a8b5882
sujjangOvO <89267864+sujjangOvO@users.noreply.github.com>
2023-05-05 21:33:07 +0900

    [unrated] Title: 흉부외과 또는 일반외과 의사 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ 프로그래머스/unrated/132203. 흉부외과 또는 일반외과 의사 목록 출력하기/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -1,5 +1,9 @@
 -- 코드를 입력하세요
 SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
 FROM DOCTOR
-WHERE MCDP_CD = 'CS' OR MCDP_CD = 'GS' -- MCDP_CD IN ('CS', 'GS')
+WHERE MCDP_CD IN ('CS', 'GS') -- MCDP_CD = 'CS' OR MCDP_CD = 'GS' 
 ORDER BY HIRE_YMD DESC, DR_NAME;
+
+
+-- Mysql 문자열 비교는 == 가 아니라 = 이다.
+-- 다중정렬 할때는 ,(콤마) 를 빼먹지 말고 적어주자
