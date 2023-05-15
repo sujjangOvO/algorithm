a5f2d3201c4ca61e3c0d22b82a45bd4891cfe8d0
sujjangOvO <89267864+sujjangOvO@users.noreply.github.com>
2023-05-16 01:24:20 +0900

    [unrated] Title: 자동차 대여 기록에서 장기/단기 대여 구분하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
+++ 프로그래머스/unrated/151138. 자동차 대여 기록에서 장기／단기 대여 구분하기/자동차 대여 기록에서 장기／단기 대여 구분하기.sql
@@ -1,4 +1,7 @@
--- 코드를 입력하세요
+-- DATEDIFF(날짜1, 날짜2): 날짜 차이(날짜1-날짜2) 가져오는 함수 
+-- +1 해주는 이유는 반납일과 시작일이 같으면 0으로 카운트 되므로 이를 방지하기 위해 +1
+-- 그 외, TIMESTAMPDIFF 함수로 다양한 단위의 날짜 계산도 할 수 있다
+
 SELECT HISTORY_ID, CAR_ID,
     DATE_FORMAT(START_DATE, '%Y-%m-%d') AS START_DATE,
     DATE_FORMAT(END_DATE, '%Y-%m-%d') AS END_DATE,
