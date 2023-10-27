203435deb851fd4f9dd590b32ea5aca6d192e2ea
수짱 <ssu7038@yu.ac.kr>
2023-10-28 00:35:05 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.15 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for(int i=1; i<food.length; i++) {
+            food[i] = food[i] / 2;
+            
+            for(int j=0; j<food[i]; j++) {
+                answer += i;
+            }
+        }
+        
+        answer += 0;
+        
+        for(int i=food.length-1; i>0; i--) {
+            for(int j=0; j<food[i]; j++) {
+                answer += i;
+            }
+        }
+        
+        return answer;
+    }
+}
