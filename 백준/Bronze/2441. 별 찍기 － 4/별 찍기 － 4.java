7912880e1f88a577d7e96bc353c6a40208063ba8
수짱 <ssu7038@yu.ac.kr>
2024-01-24 16:18:35 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 360 ms, Memory: 18400 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i=n; i>0; i--) {
+
+            for(int j=n-i; j>0; j--) {
+                System.out.print(" ");
+            }
+
+            for(int j=i; j>0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
