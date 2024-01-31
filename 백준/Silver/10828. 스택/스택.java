1b137519c39326cb7060103246c1d9eb02889ca7
수짱 <ssu7038@yu.ac.kr>
2024-01-31 19:39:29 +0900

    [Silver IV] Title: 스택, Time: 216 ms, Memory: 19328 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,14 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    public static int[] stack;
+    public static int size = 0;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-            Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        stack = new int[n];
 
         for (int i = 0; i < n; i++) {
             String str = br.readLine();
@@ -17,39 +21,54 @@ public class Main {
             switch (st.nextToken()) {
                 case "push":
                     int value = Integer.valueOf(st.nextToken());
-                    stack.push(value);
+                    push(value);
                     break;
 
                 case "pop":
-                    if(stack.empty()) {
-                        System.out.println(-1);
-                    } else {
-                        System.out.println(stack.pop());
-                    }
+                    sb.append(pop() + "\n");
                     break;
 
                 case "size":
-                    System.out.println(stack.size());
+                    sb.append(size() + "\n");
                     break;
 
                 case "empty":
-                    if (stack.empty()) {
-                        System.out.println(1);
-                    } else {
-                        System.out.println(0);
-                    }
+                    sb.append(empty() + "\n");
                     break;
 
                 case "top":
-                    if(stack.empty()) {
-                        System.out.println(-1);
-                    } else {
-                        System.out.println(stack.peek());
-                    }
+                    sb.append(top() + "\n");
                     break;
             }
         }
-
+        System.out.println(sb);
         br.close();
+    }
+
+    public static void push(int n) {
+        stack[size++] = n;
+    }
+
+    public static int pop() {
+        if(size == 0) {
+            return -1;
+        } else {
+            int popValue = stack[size-1];
+            stack[size-1] = 0;
+            size--;
+            return popValue;
+        }
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        return (size == 0) ? 1 : 0;
+    }
+
+    public static int top() {
+        return (size ==  0) ? -1 : stack[size-1];
     }
 }
